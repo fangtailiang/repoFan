7b9045414bd698b664dd391ef47efe93761c0a4a
tangrong <772504473@qq.com>
2023-05-17 10:57:10 +0800

    add

--- __tests__/calc.spec.ts
+++ __tests__/calc.spec.ts
@@ -1,5 +1,6 @@
-import calc from '../src'
+import { calc, add } from '../src'
 
 test('The calculation result should be 996.', () => {
   expect(calc(1024, 28)).toBe(996)
+  expect(add(12, 5)).toBe(17)
 })
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 const calc = (a: number, b: number): number => {
   return a - b
 }
-export default calc
+const add = (a: number, b: number): number => {
+  return a + b
+}
+export { calc, add }
