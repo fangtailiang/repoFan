773014e11fec70b4a9dc07285d51589b543ce9ba
tangrong <772504473@qq.com>
2023-08-10 15:19:34 +0800

    test:报错

--- __tests__/calc.spec.ts
+++ __tests__/calc.spec.ts
@@ -2,5 +2,5 @@ import { Validators, RegExps } from '../src/index'
 
 test('The calculation result should be utils.', () => {
   expect(Validators.validatorFun(RegExps.password, 'fgdsfg445')).toBe(true)
-  expect(Validators.validatorNull('    ')).toBe(true)
+  expect(Validators.validatorNull('    ')).toBe(false)
 })
